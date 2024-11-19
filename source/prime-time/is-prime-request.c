a7405cdca4f97961d8adeb313ed23dbe12225963
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-11-19 05:55:49 -0500

    feat: is prime coming together

--- source/prime-time/is-prime-request.c
+++ source/prime-time/is-prime-request.c
@@ -1,2 +1,7 @@
 
 #include "prime-time/is-prime-request.h"
+
+int is_prime_request_builder(struct is_prime_request **request, const char* raw_request, size_t req_size)
+{
+  /*assert(re)*/
+}
--- source/prime-time/is-prime-request.h
+++ source/prime-time/is-prime-request.h
@@ -8,17 +8,19 @@
 #define PRIME_RESPONSE_METHOD_KEY "method"
 #define PRIME_RESPONSE_METHOD_VALUE "isPrime"
 #define PRIME_RESPONSE_NUMBER_KEY "prime"
-#define PRIME_RESPONSE_ILLFORMED "{\"method\":\"isPrime\",\"prime\":ill-formed!}"
+#define PRIME_RESPONSE_ILLFORMED "{\"method\":\"isPrime\",\"prime\":ill-formed-request!}"
 
 struct is_prime_request {
   char raw_request[PRIME_MAX_REQUEST_SIZE];
   int is_malformed;   // 0 or 1
   char response[PRIME_MAX_RESPONSE_SIZE];
   int is_prime;   // 0 or 1
+  struct is_prime_request* next;
 };
 
-void is_prime_request_builder(struct is_prime_request *request, const char* raw_request, size_t req_size);
-void is_prime_request_malformed(struct is_prime_request *request);
-void is_prime(struct is_prime_request *request);
-void is_prime_beget_response(struct is_prime_request *request);
+int is_prime_request_builder(struct is_prime_request **request, const char* raw_request, size_t req_size);
+int is_prime_request_malformed(struct is_prime_request *request);
+int is_prime(struct is_prime_request *request);
+int is_prime_beget_response(struct is_prime_request *request);
+int is_prime_free(struct is_prime_request *request);
 
