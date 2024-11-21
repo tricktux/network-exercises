e7c8b29d1cabe9dad7c4858a5176215b211515b5
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-11-21 06:06:54 -0500

    feat: initial implementation of super awesome network queue

---
+++ source/utils/queue.c
@@ -0,0 +1,101 @@
+
+
+#include <bits/types/struct_iovec.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <time.h>
+#include <sys/time.h>
+
+#include "utils/queue.h"
+#include "log/log.h"
+
+void queue_init(struct queue *qu, size_t capacity)
+{
+  if (qu != NULL) {
+    log_error("queue_init: queue was previously allocated");
+    return;
+  }
+
+  assert(capacity > 0);
+
+  qu = malloc(sizeof(struct queue));
+  qu->data = malloc(capacity);
+  qu->capacity = capacity;
+  qu->free_capacity = capacity;
+  qu->size = 0;
+  qu->head = qu->data;
+}
+
+void queue_free(struct queue *qu)
+{
+  if (qu == NULL) {
+    log_error("queue_free: queue was previously de-allocated");
+    return;
+  }
+
+  assert(qu->data != NULL);
+  free(qu->data);
+  free(qu);
+  qu = NULL;
+}
+
+void queue_push(struct queue *qu, char* data, size_t size)
+{
+  assert(qu != NULL);
+  assert(data != NULL);
+
+  if (size == 0) {
+    log_warn("queue_push: size is zero... noop");
+    return;
+  }
+
+  if (qu->size + size >= qu->capacity) {
+    log_error("queue_push: pushing over queue capacity... noop");
+    return;
+  }
+
+  memcpy(qu->head, data, size);
+  qu->head += size;
+  qu->size += size;
+  qu->free_capacity = qu->capacity - qu->size;
+}
+
+void queue_push_ex(struct queue *qu, size_t size)
+{
+  assert(qu != NULL);
+
+  if (size == 0) {
+    log_warn("queue_push: size is zero... noop");
+    return;
+  }
+
+  if (qu->size + size >= qu->capacity) {
+    log_error("queue_push: pushing over queue capacity... noop");
+    return;
+  }
+
+  qu->head += size;
+  qu->size += size;
+  qu->free_capacity = qu->capacity - qu->size;
+}
+
+void queue_pop(struct queue *qu, char* data, size_t *size)
+{
+  assert(qu != NULL);
+  assert(data != NULL);
+  assert(size != NULL);
+
+  if (qu->size == 0) {
+    log_warn("queue_pop: queue is empty... noop");
+    return;
+  }
+
+  memcpy(data, qu->data, qu->size);
+  qu->size = 0;
+  qu->head = qu->data;
+  qu->free_capacity = qu->capacity;
+}
---
+++ source/utils/queue.h
@@ -0,0 +1,13 @@
+
+struct queue {
+  char *data;
+  size_t capacity;
+  size_t size;
+  size_t free_capacity;
+  char *head;
+};
+
+void queue_init(struct queue *qu, size_t capacity);
+void queue_push(struct queue *qu, char* data, size_t size);
+void queue_pop(struct queue *qu, char* data, size_t *size);
+void queue_free(struct queue *qu);
