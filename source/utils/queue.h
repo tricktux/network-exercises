61241d2637382f6be905d367bb58c7a7330a4090
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-11-22 05:07:59 -0500

    fix: allow these to be called from cpp

--- source/utils/queue.h
+++ source/utils/queue.h
@@ -1,4 +1,8 @@
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 struct queue {
   char *data;
   size_t capacity;
@@ -9,5 +13,10 @@ struct queue {
 
 void queue_init(struct queue *qu, size_t capacity);
 void queue_push(struct queue *qu, char* data, size_t size);
+void queue_push_ex(struct queue *qu, size_t size);
 void queue_pop(struct queue *qu, char* data, size_t *size);
 void queue_free(struct queue *qu);
+
+#ifdef __cplusplus
+}
+#endif
