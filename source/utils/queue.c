8738334f19f0ad5e9cea97585bbd8124caf7a572
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-11-22 22:24:03 -0500

    docs: add interface documentation
    - Admittedly AI generated

--- source/utils/queue.c
+++ source/utils/queue.c
@@ -13,6 +13,15 @@
 #include "utils/queue.h"
 #include "log/log.h"
 
+/**
+ * @brief Initializes a queue with the specified capacity.
+ *
+ * This function allocates memory for a queue structure and initializes it 
+ * with given capacity. If the queue is already allocated, an error is logged.
+ *
+ * @param qu A pointer to a pointer to the queue to be initialized.
+ * @param capacity The maximum number of bytes the queue can hold. Must be greater than 0.
+ */
 void queue_init(struct queue** qu, size_t capacity)
 {
   if (*qu != NULL) {
@@ -34,6 +43,14 @@ void queue_init(struct queue** qu, size_t capacity)
   (*qu)->head = (*qu)->data;
 }
 
+/**
+ * @brief Frees the memory allocated for the queue.
+ *
+ * This function deallocates the memory for the queue structure and its data. 
+ * If the queue is already freed, an error is logged.
+ *
+ * @param qu A pointer to a pointer to the queue to be deallocated.
+ */
 void queue_free(struct queue** qu)
 {
   if (*qu == NULL) {
@@ -46,6 +63,17 @@ void queue_free(struct queue** qu)
   *qu = NULL;
 }
 
+/**
+ * @brief Pushes data onto the queue.
+ *
+ * This function adds a specified amount of data to the queue if there is 
+ * enough capacity. If the queue is full or the data size is zero, it logs 
+ * an appropriate warning or error.
+ *
+ * @param qu A pointer to the queue where the data will be added.
+ * @param data A pointer to the data to be pushed onto the queue.
+ * @param size The size of the data to be pushed onto the queue.
+ */
 void queue_push(struct queue* qu, char* data, size_t size)
 {
   assert(qu != NULL);
@@ -67,6 +95,16 @@ void queue_push(struct queue* qu, char* data, size_t size)
   qu->free_capacity = qu->capacity - qu->size;
 }
 
+/**
+ * @brief Pushes an empty space of a specified size onto the queue.
+ *
+ * This function reserves space in the queue without copying data. It should 
+ * increase the size of the queue by the specified size, provided that there 
+ * is enough capacity.
+ *
+ * @param qu A pointer to the queue where the space will be reserved.
+ * @param size The size of the space to be reserved.
+ */
 void queue_push_ex(struct queue* qu, size_t size)
 {
   assert(qu != NULL);
@@ -86,6 +124,18 @@ void queue_push_ex(struct queue* qu, size_t size)
   qu->free_capacity = qu->capacity - qu->size;
 }
 
+/**
+ * @brief Pops data from the queue into the provided buffer.
+ *
+ * This function copies the current data from the queue into the provided 
+ * buffer. It resets the queue's size to zero afterward. If there is 
+ * insufficient space or the queue is empty, it logs warnings.
+ *
+ * @param qu A pointer to the queue from which data will be popped.
+ * @param data A pointer to the buffer where popped data will be stored.
+ * @param size A pointer to a size variable that will be updated with 
+ *             the size of the data popped from the queue.
+ */
 void queue_pop(struct queue* qu, char* data, size_t* size)
 {
   assert(qu != NULL);
@@ -110,6 +160,14 @@ void queue_pop(struct queue* qu, char* data, size_t* size)
   qu->free_capacity = qu->capacity;
 }
 
+/**
+ * @brief Resets the queue to its initial state.
+ *
+ * This function sets the size of the queue to zero and resets the head 
+ * pointer to the data pointer.
+ *
+ * @param qu A pointer to the queue to be reset.
+ */
 void queue_reset(struct queue* qu)
 {
   assert(qu != NULL);
@@ -119,6 +177,18 @@ void queue_reset(struct queue* qu)
   qu->free_capacity = qu->capacity;
 }
 
+/**
+ * @brief Pops data from the queue without copying.
+ *
+ * This function retrieves a pointer to the queue data without copying 
+ * it to another buffer. After calling this function, the queue is reset 
+ * to zero size. If the queue is empty, it logs a warning.
+ *
+ * @param qu A pointer to the queue from which data will be popped.
+ * @param data A pointer to a pointer where the address of the queue data 
+ *             will be stored.
+ * @return The size of the data in the queue (0 if empty).
+ */
 int queue_pop_no_copy(struct queue* qu, char** data)
 {
   assert(qu != NULL);
