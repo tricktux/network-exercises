cf4a991e71d4a67c7277a72d5f4d1d9b5ce2c936
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-11-24 22:19:35 -0500

    tests!: major refactor not to mix tests
    - Each interface get's it's own tests executable
    - Otherwise the tests source file will get very large
    - There's prob another way to do this

---
+++ test/is-prime/is-prime-request-test.cpp
@@ -0,0 +1,59 @@
+
+#define CATCH_CONFIG_MAIN
+#include <bits/types/struct_iovec.h>
+#include <assert.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <json-c/json.h>
+#include <json-c/json_tokener.h>
+
+#include <catch2/catch.hpp>
+#include "prime-time/is-prime-request.h"
+
+TEST_CASE("is_prime_request_builder handles valid requests", "[request]")
+{
+  struct is_prime_request* request = NULL;
+
+  SECTION("Valid request with prime number")
+  {
+    char raw_request[] = "{\"method\":\"isPrime\",\"number\":17}\n";
+    REQUIRE(is_prime_request_builder(&request, raw_request, strlen(raw_request))
+            == 1);
+    REQUIRE(request != NULL);
+    REQUIRE(request->number == 17);
+    REQUIRE(request->is_prime == true);
+  }
+
+  SECTION("Valid request with non-prime number")
+  {
+    char raw_request[] = "{\"method\":\"isPrime\",\"number\":24}\n";
+    REQUIRE(is_prime_request_builder(&request, raw_request, strlen(raw_request))
+            == 1);
+    REQUIRE(request != NULL);
+    REQUIRE(request->number == 24);
+    REQUIRE(request->is_prime == false);
+  }
+
+  /*SECTION("Valid request with floating-point number") {*/
+  /*  char raw_request[] = "{\"method\":\"isPrime\",\"number\":17.5}\n";*/
+  /*  REQUIRE(is_prime_request_builder(&request, raw_request,
+   * strlen(raw_request)) == 1);*/
+  /*  REQUIRE(request != NULL);*/
+  /*  REQUIRE(request->number < 0);*/
+  /*  REQUIRE(request->is_prime == false);*/
+  /*}*/
+
+  if (request)
+    is_prime_free(&request);
+}
--- test/source/network-exercises_test.cpp
+++
@@ -1,6 +0,0 @@
-#include <memory>
-#include <string>
-
-#include <catch2/catch.hpp>
-
-TEST_CASE("Name is network-exercises", "[library]") {}
