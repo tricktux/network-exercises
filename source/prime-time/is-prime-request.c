ce77a0a2c8daa15d8d1b6042e9179db24822ec7f
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-11-24 20:29:48 -0500

    feat: baseline implementation of request validation

--- source/prime-time/is-prime-request.c
+++ source/prime-time/is-prime-request.c
@@ -17,6 +17,9 @@
 #include <unistd.h>
 #include <stdatomic.h>
 
+#include <json-c/json.h>
+#include <json-c/json_tokener.h>
+
 #include "log/log.h"
 
 #include "prime-time/is-prime-request.h"
@@ -31,9 +34,11 @@ int is_prime_request_builder(struct is_prime_request** request,
   assert(raw_request != NULL);
   assert(req_size > 0);
 
+  int j;
   char *str1, *token;
   char *saveptr1;
-  int j;
+  struct is_prime_request* preq;
+
   // tokenizer on the split delimiters
   for (j = 1, str1 = raw_request; ; j++, str1 = NULL) {
     token = strtok_r(str1, DELIMETERS, &saveptr1);
@@ -46,7 +51,63 @@ int is_prime_request_builder(struct is_prime_request** request,
   return 0;
 }
 
-int is_prime_request_malformed(struct is_prime_request* request) {}
+int is_prime_init(struct is_prime_request** request, char *req, size_t req_size)
+{
+
+}
+
+int is_prime_request_malformed(char *req) 
+{
+  assert(req != NULL);
+
+  log_trace("is_prime_request_malformed: parsing request: '%s'", req);
+
+  // Is json?
+  json_object *root = json_tokener_parse(req);
+  if (!root) {
+    log_warn("is_prime_request_malformed: json_tokener_parse failed");
+    return -1;
+  }
+
+  json_object *method = json_object_object_get(root, PRIME_RESPONSE_METHOD_KEY);
+  if (!method) {
+    json_object_put(root);
+    log_warn("is_prime_request_malformed: json_object_object_get failed for 'method'");
+    return -2;
+  }
+
+  const char *method_value = json_object_get_string(method);
+  if (!method_value) {
+    json_object_put(root);
+    log_warn("is_prime_request_malformed: json_object_get_string failed for 'method'");
+    return -3;
+  }
+
+  if (strncmp(PRIME_RESPONSE_METHOD_VALUE, method_value, PRIME_RESPONSE_METHOD_VALUE_LEN) != 0) {
+    json_object_put(root);
+    log_warn("is_prime_request_malformed: method value did not match '%s'", method_value);
+    return -4;
+  }
+
+  json_object *number = json_object_object_get(root, PRIME_REQUEST_NUMBER_KEY);
+  if (!method) {
+    json_object_put(root);
+    log_warn("is_prime_request_malformed: json_object_object_get failed for 'number'");
+    return -5;
+  }
+
+  errno = 0;
+  int number_value = json_object_get_int(number);
+  if (errno != 0) {
+    json_object_put(root);
+    log_warn("is_prime_request_malformed: json_object_get_int64 failed for '%s'", number);
+    return -7;
+  }
+
+  json_object_put(root);
+  return number_value;
+}
+
 int is_prime(struct is_prime_request* request) {}
 char* is_prime_beget_response(struct is_prime_request* request) {}
-int is_prime_free(struct is_prime_request* request) {}
+int is_prime_free(struct is_prime_request** request) {}
--- source/prime-time/is-prime-request.h
+++ source/prime-time/is-prime-request.h
@@ -1,27 +1,29 @@
 
 #define PRIME_MAX_REQUEST_SIZE 8192
-#define PRIME_MAX_RESPONSE_SIZE 8192
+#define PRIME_MAX_RESPONSE_SIZE PRIME_MAX_REQUEST_SIZE
 #define PRIME_REQUEST_METHOD_KEY "method"
 #define PRIME_REQUEST_METHOD_VALUE "isPrime"
 #define PRIME_REQUEST_NUMBER_KEY "number"
 #define PRIME_RESPONSE_METHOD_KEY "method"
 #define PRIME_RESPONSE_METHOD_VALUE "isPrime"
+#define PRIME_RESPONSE_METHOD_VALUE_LEN 7
 #define PRIME_RESPONSE_NUMBER_KEY "prime"
 #define PRIME_RESPONSE_ILLFORMED \
   "{\"method\":\"isPrime\",\"prime\":ill-formed-request!}"
 
 struct is_prime_request {
-  char raw_request[PRIME_MAX_REQUEST_SIZE];
-  int is_malformed;  // 0 or 1
   char response[PRIME_MAX_RESPONSE_SIZE];
+  int is_malformed;  // 0 or 1
   int is_prime;  // 0 or 1
+  int number;  // 0 or 1
   struct is_prime_request* next;
 };
 
 int is_prime_request_builder(struct is_prime_request** request,
                              char* raw_request,
                              size_t req_size);
-int is_prime_request_malformed(struct is_prime_request* request);
+int is_prime_request_malformed(char *req);
 int is_prime(struct is_prime_request* request);
 char* is_prime_beget_response(struct is_prime_request* request);
-int is_prime_free(struct is_prime_request* request);
+int is_prime_init(struct is_prime_request** request, char *req, size_t req_size);
+int is_prime_free(struct is_prime_request** request);
