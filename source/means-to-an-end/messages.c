e1c3041f726bd90f7ab215dcab6a9739902ac222
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2024-12-04 23:19:49 -0500

    fix: bug missing k++

--- source/means-to-an-end/messages.c
+++ source/means-to-an-end/messages.c
@@ -1,20 +1,25 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <assert.h>
 #include <arpa/inet.h>
 
 #include "utils/queue.h"
 #include "means-to-an-end/asset-prices.h"
 #include "means-to-an-end/messages.h"
 
-void message_parse(struct queue* sdqu, char* data, size_t dsize)
+void message_parse(struct asset_price *ps, struct queue* sdqu, char* data, size_t dsize)
 {
-  // TODO: create and allocate struct prices
+  assert(ps != NULL);
+  assert(sdqu != NULL);
+  assert(data != NULL);
+  assert(dsize > 0);
+
   char* pd = data;
   int k;
   int num_msgs = dsize / MESSAGE_SIZE;
-  for (k = 0; k < num_msgs; pd += MESSAGE_SIZE) {
-    char type = *pd;
+  for (k = 0; k < num_msgs; k++, pd += MESSAGE_SIZE) {
+    char type = pd[0];
     uint32_t* first_word = (uint32_t*)(pd + MESSAGE_FIRST_WORD_OFFSET);
     uint32_t* second_word = (uint32_t*)(pd + MESSAGE_SECOND_WORD_OFFSET);
 
@@ -23,18 +28,16 @@ void message_parse(struct queue* sdqu, char* data, size_t dsize)
         struct asset_price_query qry;
         qry.mintime = ntohl(*first_word);
         qry.maxtime = ntohl(*second_word);
-        // Process query...
+        int32_t mean_p = htonl(asset_prices_query(ps, &qry));
+        queue_push(sdqu, (char *) &mean_p, sizeof(int32_t));
         break;
       }
       case MESSAGE_INSERT: {
         struct asset_price prc;
         prc.timestamp = ntohl(*first_word);
         prc.price = ntohl(*second_word);
-        // Insert price directly into vector without additional memcpy
-        /*if (p->size == p->capacity) {*/
-        // Resize logic here
-        /*}*/
-        /*p->data[p->size++] = prc;*/
+        if (!asset_prices_duplicate_timestamp_check(ps, prc.timestamp))
+          asset_prices_push(ps, &prc);
         break;
       }
       default:
--- source/means-to-an-end/messages.h
+++ source/means-to-an-end/messages.h
@@ -8,6 +8,6 @@
 #define MESSAGE_FIRST_WORD_OFFSET 1  // These are in network byte order
 #define MESSAGE_SECOND_WORD_OFFSET 5  // These are in network byte order
 
-void message_parse(struct queue* sdqu, char* data, size_t dsize);
+void message_parse(struct asset_price *ps, struct queue* sdqu, char* data, size_t dsize);
 
 #endif  // INCLUDE_MESSAGES_H_
