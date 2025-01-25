a5eba8a27402e082a6fb6bd9ec04080035a3a1cd
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2025-01-25 07:32:31 -0500

    fix!: smoke test passing

--- zig/src/0-smoke-test/main.zig
+++ zig/src/0-smoke-test/main.zig
@@ -3,15 +3,17 @@ const linux = std.os.linux;
 const nexlog = @import("nexlog");
 const debug = std.debug.print;
 
+// Constants
+const name: []const u8 = "0.0.0.0";
+const port = 18888;
+const buff_size = 1048576;
+
 pub fn main() !void {
     // Initialize allocator
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
-    // Constants
-    const name: []const u8 = "0.0.0.0";
-    const port = 18888;
-    const buff_size = 1048576;
+    const allocator = gpa.allocator();
 
     // Create server
     var server: std.net.Server = undefined;
@@ -30,26 +32,38 @@ pub fn main() !void {
         }
     }
 
-    var data: [buff_size]u8 = undefined;
+    const cpus = try std.Thread.getCpuCount();
+    var tp: std.Thread.Pool = undefined;
+    try tp.init(.{ .allocator = allocator, .n_jobs = @as(u32, @intCast(cpus)) });
+    defer tp.deinit();
 
+    debug("ThreadPool initialized with {} capacity\n", .{cpus});
     debug("We are listeninig baby!!!...\n", .{});
     while (true) {
         debug("waiting for a new connection...\n", .{});
         const connection = try server.accept();
-
         debug("got new connection!!!\n", .{});
-        while (true) {
-            debug("\twaiting for some data...\n", .{});
-            const stream = connection.stream;
-            const bytes = stream.read(&data) catch 0;
-            if (bytes == 0) {
-                debug("\tERROR: error, or closing request either way... closing this connection\n", .{});
-                stream.close();
-                break;
-            }
+        try tp.spawn(handle_connection, .{connection});
+    }
+}
 
-            debug("\treceived some data.len = {}!!\n", .{data.len});
-            try stream.writeAll(&data);
+fn handle_connection(connection: std.net.Server.Connection) void {
+    var data: [buff_size]u8 = undefined;
+    const stream = connection.stream;
+    defer stream.close();
+
+    while (true) {
+        debug("\twaiting for some data...\n", .{});
+        const bytes = stream.read(&data) catch 0;
+        if (bytes == 0) {
+            debug("\tERROR: error, or closing request either way... closing this connection\n", .{});
+            return;
         }
+
+        debug("\treceived some bytes = {}!!\n", .{bytes});
+        stream.writeAll(data[0..bytes]) catch |err| {
+            debug("\tERROR: error sendAll function {}... closing this connection\n", .{err});
+            return;
+        };
     }
 }
