e23bdb8e6d70f322eb936f8510a3f3412be713e5
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2025-03-09 00:46:24 -0500

    wip: udp handling

--- zig/src/4-unusual-database/main.zig
+++ zig/src/4-unusual-database/main.zig
@@ -20,36 +20,51 @@ pub fn main() !void {
     const allocator = gpa.allocator();
 
     // Create server
-    var server: std.net.Server = undefined;
-    defer server.deinit();
-    {
-        const addrlist = try std.net.getAddressList(allocator, name, port);
-        defer addrlist.deinit();
-        debug("Got Addresses: '{s}'!!!\n", .{addrlist.canon_name.?});
+    _ = try std.net.Address.parseIp(name, port);
+    const sock = try std.posix.socket(std.posix.AF.INET, std.posix.SOCK.DGRAM, std.posix.IPPROTO.UDP);
+    defer std.posix.close(sock);
 
-        for (addrlist.addrs) |addr| {
-            debug("\tTrying to listen...\n", .{});
-            // Not intuitive but `listen` calls `socket, bind, and listen`
-            server = addr.listen(.{}) catch continue;
-            debug("\tGot one!\n", .{});
-            break;
-        }
-    }
+    var db = try Database.init(allocator);
+    var buff: [1024]u8 = undefined;
 
-    // Initialize thread pool
-    const cpus = try std.Thread.getCpuCount();
-    var tp: std.Thread.Pool = undefined;
-    try tp.init(.{ .allocator = allocator, .n_jobs = @as(u32, @intCast(cpus)) });
-    defer tp.deinit();
-
-    debug("ThreadPool initialized with {} capacity\n", .{cpus});
-    debug("We are listeninig baby!!!...\n", .{});
-    const thread_id = std.Thread.getCurrentId();
+    var sa: std.net.Address = undefined;
+    const sl: std.posix.socklen_t = @sizeOf(std.posix.sockaddr.in);
+    var sl_copy = sl;
     while (true) {
-        debug("INFO({d}): waiting for a new connection...\n", .{thread_id});
-        const connection = try server.accept();
-        debug("INFO({d}): got new connection!!!\n", .{thread_id});
-        try tp.spawn(handle_connection, .{ connection, allocator });
+        @memset(@as([*]u8, @ptrCast(&sa))[0..@sizeOf(std.net.Address)], 0);
+        sl_copy = sl;
+        const result = std.posix.recvfrom(sock, &buff, 0, &sa.any, &sl_copy) catch |err| {
+            debug("ERROR: recvfrom failed: {!}\n", .{err});
+            return err;
+        };
+
+        if (result == 0) continue;
+
+        const resp = buff[0..result];
+        const eqidx = std.mem.indexOf(u8, resp, "=");
+        if (eqidx) |idx| {
+            // TODO: Insert request
+            const key = resp[0..idx];
+            const value = resp[idx..];
+            db.insert(key, value) catch |err| {
+                debug("ERROR: inserting key: '{s}', value: '{s}', error: {!}\n", .{key, value, err});
+                continue;
+            };
+        } else {
+            // TODO: Retrieve request
+            const value = db.retrieve(resp) catch |err| {
+                debug("ERROR: retrieving key: '{s}', error: {!}\n", .{resp, err});
+                continue;
+            };
+
+            _ = value;
+            // if (value) |val| {
+            //     // TODO: format response '{s}={s}', .{resp, value}
+            //     // TODO: Use this to respond
+            //     // _ = posix.sendto(fd, queries[i], posix.MSG.NOSIGNAL, &ns[j].any, sl) catch undefined;
+            // }
+        }
+
     }
 }
 
@@ -104,60 +119,6 @@ const Database = struct {
         return r;
     }
 };
-
-fn handle_connection(connection: std.net.Server.Connection, alloc: std.mem.Allocator) void {
-    const thread_id = std.Thread.getCurrentId();
-
-    const stream = connection.stream;
-    defer stream.close();
-
-    var recv_fifo = std.fifo.LinearFifo(u8, .Dynamic).init(alloc);
-    defer recv_fifo.deinit();
-
-    stream.writeAll("Welcome to budgetchat! What shall I call you?\n") catch |err| {
-        debug("\t\tERROR({d}): error sendAll function {!}... closing this connection\n", .{ thread_id, err });
-        return;
-    };
-
-    var msg_buffer = std.BoundedArray(u8, 1024).init(0) catch |err| {
-        debug("\tERROR({d}): error while initializing msg_buffer: {!}\n", .{ thread_id, err });
-        return;
-    };
-
-    while (true) {
-        debug("\tINFO({d}): waiting for some data...\n", .{thread_id});
-        const data = recv_fifo.writableWithSize(std.mem.page_size * 4) catch |err| {
-            debug("\tERROR({d}): error while recv_fifo.writableWithSize: {!}\n", .{ thread_id, err });
-            return;
-        };
-
-        const bytes = stream.read(data) catch |err| {
-            debug("\tERROR({d}): error {!}... closing this connection\n", .{ thread_id, err });
-            return;
-        };
-        if (bytes == 0) {
-            debug("\t\tWARN({d}): Client closing this connection\n", .{
-                thread_id,
-            });
-            return;
-        }
-
-        recv_fifo.update(bytes);
-
-        // Check if we have a full message
-        const datapeek = recv_fifo.readableSlice(0);
-        // TODO: Maybe switch to using readUntilDelimiterOrEnd
-        // Danger here of sending out more than one message
-        const idx = std.mem.lastIndexOf(u8, datapeek, needle);
-        if (idx == null) continue;
-
-        // Clean up
-        defer recv_fifo.discard(recv_fifo.readableLength());
-        defer msg_buffer.clear();
-
-        // const msg = datapeek[0..idx.?];
-    }
-}
 
 test "StringArrayHashMap" {
     const allocator = std.testing.allocator;
