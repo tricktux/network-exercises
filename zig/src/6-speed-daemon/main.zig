5ab9280b829cded93229bdb9cb2e6b76c4ad823e
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2025-04-04 04:41:22 -0400

    wip: handling messages

--- zig/src/6-speed-daemon/main.zig
+++ zig/src/6-speed-daemon/main.zig
@@ -151,8 +151,6 @@ fn handle_events(ctx: *Context, serverfd: socketfd, alloc: std.mem.Allocator) vo
         std.log.debug("got '{d}' events", .{ready_count});
         for (ready_events.items[0..ready_count]) |event| {
             const ready_socket = event.data.fd;
-            // const stream = std.net.Stream{ .handle = ready_socket };
-
             // TODO: even timers need this?
             defer ctx.epoll.mod(ready_socket) catch |err| {
                 std.log.err("Failed to re-add socket to epoll: {!}", .{err});
@@ -210,9 +208,120 @@ fn handle_events(ctx: *Context, serverfd: socketfd, alloc: std.mem.Allocator) vo
                 continue;
             }
 
+            // Then it must be we got a new message
             std.log.debug("({d}): got new message!!!", .{thread_id});
             // TODO: do something
-            // try tp.spawn(handle_messge, .{ &map, ctx });
+            // - Read the messages
+            const stream = std.net.Stream{ .handle = ready_socket };
+            const client = ctx.clients.get(ready_socket);
+
+            // TODO: Need fifo here
+            var bytes: usize = 0;
+            var read_error = false;
+            while (true) {
+                bytes = stream.read(buf.buffer[bytes..]) catch |err| {
+                    switch (err) {
+                        error.WouldBlock => break,
+                        else => {
+                            std.log.err("error while reading from client: {!}", .{err});
+                            read_error = true;
+                            break;
+                        },
+                    }
+                };
+                if (bytes == 0) break;
+                if (bytes >= 2048) {
+                    std.log.err("Too many bytes read: {d}", .{bytes});
+                    break;
+                }
+                buf.len += bytes;
+            }
+
+            if (read_error) {
+                std.log.err("({d}): error while reading from client: {!}", .{thread_id, ready_socket});
+                if (client != null) {
+                    ctx.clients.del(ready_socket, ctx.epoll) catch |err| {
+                        std.log.err("Failed to del client: {!}", .{err});
+                    };
+                }
+                ctx.epoll.del(ready_socket) catch |err| {
+                    std.log.err("Failed to del client: {!}", .{err});
+                };
+                continue;
+            }
+
+            // - Handle read zero byte
+            if (bytes == 0) {
+                if (client != null) {
+                    std.log.debug("({d}): Client disconnected: {d}", .{thread_id, ready_socket});
+                    ctx.clients.del(ready_socket, ctx.epoll) catch |err| {
+                        std.log.err("Failed to del client: {!}", .{err});
+                    };
+                    continue;
+                }
+
+                std.log.debug("({d}): Client disconnected before identifying: {d}", .{thread_id, ready_socket});
+                ctx.epoll.del(ready_socket) catch |err| {
+                    std.log.err("Failed to del client: {!}", .{err});
+                };
+                continue;
+            }
+            // - call decode(buf, msgs)
+            _ = messages.decode(buf.constSlice(), &msgs, alloc) catch |err| {
+                std.log.err("Failed to decode messages: {!}", .{err});
+                continue;
+            };
+            // - for (msgs) |msg| { switch (msg.Type) { ... } }
+            for (&msgs.buffer) |*msg| {
+                switch (msg.type) {
+                    messages.Type.Heartbeat => {
+                        if (client == null) {
+                            std.log.err("({d}): Got heartbeat from unknown client: {d}", .{thread_id, ready_socket});
+                            continue;
+                        }
+                        std.log.debug("({d}): Got heartbeat from client: {d}", .{thread_id, ready_socket});
+                        client.heartbeat();
+                    },
+                    messages.Type.Identify => {
+                        if (client != null) {
+                            std.log.err("({d}): Got identify from already identified client: {d}", .{thread_id, ready_socket});
+                            continue;
+                        }
+                        std.log.debug("({d}): Got identify from client: {d}", .{thread_id, ready_socket});
+                        const newclient = try logic.Client.init(ready_socket, msg.identify.id, &ctx.cars, &ctx.roads, &ctx.cameras, &ctx.tickets, &ctx.epoll, &ctx.timers);
+                        ctx.clients.add(newclient);
+                    },
+                    messages.Type.GetCars => {
+                        if (client == null) {
+                            std.log.err("({d}): Got getCars from unknown client: {d}", .{thread_id, ready_socket});
+                            continue;
+                        }
+                        std.log.debug("({d}): Got getCars from client: {d}", .{thread_id, ready_socket});
+                        client.getCars();
+                    },
+                    messages.Type.GetRoads => {
+                        if (client == null) {
+                            std.log.err("({d}): Got getRoads from unknown client: {d}", .{thread_id, ready_socket});
+                            continue;
+                        }
+                        std.log.debug("({d}): Got getRoads from client: {d}", .{thread_id, ready_socket});
+                        client.getRoads();
+                    },
+                    messages.Type.GetCameras => {
+                        if (client == null) {
+                            std.log.err("({d}): Got getCameras from unknown client: {d}", .{thread_id, ready_socket});
+                            continue;
+                        }
+                        std.log.debug("({d}): Got getCameras from client: {d}", .{thread_id, ready_socket});
+                        client.getCameras();
+                    },
+                    messages.Type.GetTickets => {
+                        if (client == null) {
+                            std.log.err("({d}): Got getTickets from unknown client: {d}", .{thread_id, ready_socket});
+                        }
+                    }
+                }
+            }
         }
     }
 }
